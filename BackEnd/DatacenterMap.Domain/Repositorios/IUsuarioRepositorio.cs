6a6b91b31241a75ccd4ee9aeea81e14eb792bef4
Stéffano Lopes Heckler <steffano.heckler@cwi.com.br>
2017-12-07 18:08:34 -0200

    feat(Testes): Criado Repositorio de Mock.

---
+++ BackEnd/DatacenterMap.Domain/Repositorios/IUsuarioRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using DatacenterMap.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatacenterMap.Domain.Repositorios
+{
+	public interface IUsuarioRepositorio
+	{
+		Usuario Obter(int id);
+		List<Usuario> Listar();
+		void Criar(Usuario entidade);
+		void Excluir(int id);
+	}
+}
+}
