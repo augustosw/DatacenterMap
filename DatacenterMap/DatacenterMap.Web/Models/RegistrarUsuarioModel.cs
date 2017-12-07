1658c41451736dccd796d61f479fb258c2676a7f
Augusto Wartchow <augusto.wartchow@gmail.com>
2017-12-07 14:21:14 -0200

    fix: solution

--- DatacenterMap/DatacenterMap.Web/Models/RegistrarUsuarioModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DatacenterMap.Web.Models
-{
-    public class RegistrarUsuarioModel
-    {
-        public string Email { get; set; }
-        public string Nome { get; set; }
-        public string Senha { get; set; }
-    }
-}
