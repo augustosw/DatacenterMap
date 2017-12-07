22bd865ce6c76cdfb4be37baa97c53d9b49f6d88
João Fidellis <joao.fidellis@cwi.com.br>
2017-12-07 17:34:02 -0200

    feat: Adicionando Mapping de usuário.

---
+++ BackEnd/DatacenterMap.Infra/Mappings/UsuarioMapping.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using DatacenterMap.Domain.Entidades;
+
+namespace DatacenterMap.Infra.Mappings
+{
+    internal class UsuarioMapping : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMapping()
+        {
+            ToTable("Usuario", "DatacenterMap");
+
+            HasKey(x => x.Id);
+
+            Property(x => x.Nome).HasMaxLength(64).IsRequired();
+
+            Property(x => x.Email).HasMaxLength(128).IsRequired();
+
+            Property(x => x.Senha).HasMaxLength(255).IsRequired();
+        }
+    }
+}
