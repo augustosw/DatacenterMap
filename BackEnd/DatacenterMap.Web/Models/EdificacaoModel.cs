6c001e401cff166423ae60a7d039e032821c4a10
João Fidellis <joao.fidellis@cwi.com.br>
2017-12-07 17:10:00 -0200

    feat: Adicionando controller e model de Edificação.

---
+++ BackEnd/DatacenterMap.Web/Models/EdificacaoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DatacenterMap.Web.Models
+{
+    public class EdificacaoModel
+    {
+        public string Nome { get; set; }
+        public int NumeroAndares { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
