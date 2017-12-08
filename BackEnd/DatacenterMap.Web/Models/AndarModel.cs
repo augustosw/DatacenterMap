ec90b4db11fd69005761cea86d3edf3378900cd8
João Fidellis <joao.fidellis@cwi.com.br>
2017-12-08 01:23:56 -0200

    feat: Adicionando Controller de andar.

---
+++ BackEnd/DatacenterMap.Web/Models/AndarModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DatacenterMap.Web.Models
+{
+    public class AndarModel
+    {
+        public int Id { get; set; }
+        public int NumeroAndar { get; set; }
+        public int EdificacaoId { get; set; }
+        public int QuantidadeMaximaSalas { get; set; }
+    }
+}
