95ae730ba089b0ed9f9d151821e54e25cdf52c22
João Fidellis <joao.fidellis@cwi.com.br>
2017-12-08 01:46:15 -0200

    feat: Adicionando Controller de sala.

---
+++ BackEnd/DatacenterMap.Web/Models/SalaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DatacenterMap.Web.Models
+{
+    public class SalaModel
+    {
+        public int Id { get; set; }
+        public string NumeroSala { get; set; }
+        public int QuantidadeMaximaSlots { get; set; }
+        public double Largura { get; set; }
+        public double Comprimento { get; set; }
+        public int AndarId { get; set; } 
+    }
+}
