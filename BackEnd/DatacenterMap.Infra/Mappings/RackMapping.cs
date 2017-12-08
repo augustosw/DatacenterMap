b4c55e0372822a7d987f6e612f515beb5f68ee02
Steffano Heckler <steffano.heckler@cwi.com.br>
2017-12-08 15:38:42 -0200

    feat(mapping): criado mapping de Racks.

---
+++ BackEnd/DatacenterMap.Infra/Mappings/RackMapping.cs
@@ -0,0 +1,30 @@
+﻿using DatacenterMap.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatacenterMap.Infra.Mappings
+{
+	internal class RackMapping : EntityTypeConfiguration<Rack>
+	{
+		public RackMapping()
+		{
+			ToTable("Rack", "DatacenterMap");
+
+			HasKey(x => x.Id);
+
+			Property(x => x.QuantidadeGavetas).IsRequired();
+
+			Property(x => x.Tensao).IsRequired();
+
+			Property(x => x.Descricao).HasColumnType("varchar").HasMaxLength(255).IsRequired();
+
+			HasRequired(x => x.Slot).WithMany().Map(x => x.MapKey("Slot_Id"));
+
+			HasMany(x => x.Gavetas).WithRequired().Map(x => x.MapKey("Rack_Id"));
+		}		
+	}
+}
