faa01d191cd7533dc5a3e3a2dd0c18529d5b33d1
Gustavo Rodrigues <gustavo.rodrigues@cwi.com.br>
2017-12-08 18:47:59 -0200

    feat: adicionando css básico sidebar directive

--- FrontEnd/app.module.js
+++ FrontEnd/app.module.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute', 'ngMaterial', 'auth', 'ngMessages', 'material.svgAssetsCache', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('app', ['ngRoute', 'ngMaterial', 'auth', 'ngMessages', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 // Configurações utilizadas pelo módulo de autenticação (authService)
 angular.module('app').constant('authConfig', {
 
