dcd9ed0a278ae6ae6e5dce81286f52c715738d95
Gustavo Rodrigues <gustavo.rodrigues@cwi.com.br>
2017-12-09 04:54:12 -0200

    feat: adicionando sidebar (funcionando) - botão flutuante. necessário resolver posição do carousel

--- FrontEnd/sidebar/sidebar.directive.js
+++ FrontEnd/sidebar/sidebar.directive.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.directive('mapSidebar', function (authService, $rootScope, $log, $timeout, $mdSidenav) {
+.directive('mapSidebar', function (authService, $rootScope, $log, $timeout) {
 
   return {
 
@@ -10,16 +10,40 @@ angular.module('app')
     templateUrl: 'sidebar/sidebar.directive.html',
     
     controller: function ($scope ) {
-    
-        $scope.toggleLeft = buildToggler('left');
-        $scope.toggleRight = buildToggler('right');
-    
-        function buildToggler(componentId) {
-          return function() {
-            $mdSidenav(componentId).toggle();
-          };
+
+        $scope.isSidenavOpen = false
+        
+        $scope.toggleSidenav = function() {
+          $scope.isSidenavOpen = !$scope.isSidenavOpen
         }
+
+
+
+        //////////////////////////
+
+        $scope.active = 0;
+        var edificacoes = $scope.edificacoes = []; 
+        var slides = $scope.slides = [];
+        var currIndex = 0;
+      
+        $scope.addSlide = function() {
+          edificacoes.push({
+            image: '/image/building.jpeg',
+            text: ['building details'][slides.length % 4],
+            id: currIndex++
+          });
+        };
+      
+        for (var i = 0; i < 4; i++) {
+          $scope.addSlide();
+        }
+      
+      
+      
+
     }
+
+    
   }
 
 });
