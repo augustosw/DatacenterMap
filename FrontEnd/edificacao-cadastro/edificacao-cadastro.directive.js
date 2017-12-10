b672886bedfeaf128f26b09b8da353ee64497334
Gustavo Rodrigues <gustavo.rodrigues@cwi.com.br>
2017-12-10 05:37:47 -0200

    feat: tela cadastro de edificacoes

--- FrontEnd/edificacao-cadastro/edificacao-cadastro.directive.js
+++ FrontEnd/edificacao-cadastro/edificacao-cadastro.directive.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.directive('mapEdificacaoCadastro', function (authService, $rootScope, $log, $timeout) {
+.directive('mapEdificacaoCadastro', function (authService, edificacaoService, $rootScope, $log, $timeout) {
 
   return {
 
@@ -11,17 +11,27 @@ angular.module('app')
 
     controller: function ($scope ) {
 
+      autoComplete();
+
+      var origem;
+      var destino = {lat: -29.794918, lng: -51.146509}
+
       $scope.enviar = false;
       $scope.criar = criar;
+      $scope.fecharJanela = fecharJanela;
       $scope.edificacao = {};
+      $scope.isCheio = false;
       $scope.edificacao.numeroAndares = 1;
+      $scope.andares = [0];
       $scope.adicionar = adicionar;
       $scope.remover = remover;
+      $scope.adicionarAndar = adicionarAndar;
 
       function criar(edificacao) {
-        debugger
         if ($scope.cadastroEdificacaoForm.$valid) {      
-          //form is valid
+          // edificacaoService.criar(edificacao);
+          console.log(edificacao);
+          location.reload();
         }
         else {
            $scope.enviar = true;    
@@ -30,30 +40,53 @@ angular.module('app')
 
       function adicionar(){
         $scope.edificacao.numeroAndares++;
+        $scope.andares.push($scope.edificacao.numeroAndares);
       }
       
       function remover(){
-        if($scope.edificacao.numeroAndares==1)
-          return
+        if($scope.edificacao.numeroAndares == 1)
+          return;
         $scope.edificacao.numeroAndares--;
+        $scope.andares.splice(-1,1);
+      }
+
+      function adicionarAndar(indice) {
+            if(indice > 14){
+              $scope.isCheio = true;
+              return;
+            }
+            $scope.isCheio = false;
+            $scope.adicionando = {
+                transform:`rotateX(70deg) rotateZ(-45deg) translateZ(${indice + 2}vmin)`,
+                opacity:1 
+            }
+        return; 
+      }
+
+      function fecharJanela(){
+        $scope.fechar = {
+          display: "none"
+        }
       }
 
 
+      //api google maps autocomplete
 
-      google.maps.event.addDomListener(window, 'load', function () {  
-        var places = new google.maps.places.Autocomplete(document.getElementById('txtPlaces'));
-        google.maps.event.addListener(places, 'place_changed', function () {
-            var place = places.getPlace();
-            console.log(place);
-            var address = place.formatted_address;
-            var latitude = place.geometry.location.A;
-            var longitude = place.geometry.location.F;
-            var mesg = "Address: " + address;
-            mesg += "\nLatitude: " + latitude;
-            mesg += "\nLongitude: " + longitude;
-            alert(mesg);
-        });
-      });
+
+      function autoComplete() {
+        $scope.autocomplete = new google.maps.places.Autocomplete(
+            (document.getElementById('autocomplete'))
+        );
+        $scope.autocomplete.addListener('place_changed', obterCoordenadas);
+      }
+
+      function obterCoordenadas() {
+        var place = $scope.autocomplete.getPlace();
+        $scope.edificacao.latitude = place.geometry.location.lat();
+        $scope.edificacao.longitude = place.geometry.location.lng();
+        console.log(place);
+        return;
+      }
 
 
 
