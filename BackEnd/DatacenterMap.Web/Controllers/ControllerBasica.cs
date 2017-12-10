4c32e48b81836aa21a51e7c4f5007cd5844e361e
João Fidellis <joao.fidellis@cwi.com.br>
2017-12-10 16:24:35 -0200

    refactor: Alterando estrutura das respostas da API.

--- BackEnd/DatacenterMap.Web/Controllers/ControllerBasica.cs
+++ BackEnd/DatacenterMap.Web/Controllers/ControllerBasica.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using DatacenterMap.Domain.Entidades;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -10,22 +11,30 @@ namespace DatacenterMap.Web.Controllers
 
         public HttpResponseMessage Ok(object dados = null)
         {
-            return Request.CreateResponse(HttpStatusCode.OK, new { dados });
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new ObjectContent(dados.GetType(), dados, GlobalConfiguration.Configuration.Formatters.JsonFormatter);
+            return response;
         }
 
         public HttpResponseMessage BadRequest(List<string> mensagens)
         {
-            return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagens });
+            var response = Request.CreateResponse(HttpStatusCode.BadRequest);
+            response.Content = new ObjectContent(mensagens.GetType(), mensagens, GlobalConfiguration.Configuration.Formatters.JsonFormatter);
+            return response;
         }
 
         public HttpResponseMessage BadRequest(params string[] mensagens)
         {
-            return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagens });
+            var response = Request.CreateResponse(HttpStatusCode.BadRequest);
+            response.Content = new ObjectContent(mensagens.GetType(), mensagens, GlobalConfiguration.Configuration.Formatters.JsonFormatter);
+            return response;
         }
 
         public HttpResponseMessage BadRequest(IEnumerable<string> mensagens)
         {
-            return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagens });
+            var response = Request.CreateResponse(HttpStatusCode.BadRequest);
+            response.Content = new ObjectContent(mensagens.GetType(), mensagens, GlobalConfiguration.Configuration.Formatters.JsonFormatter);
+            return response;
         }
     }
 }
