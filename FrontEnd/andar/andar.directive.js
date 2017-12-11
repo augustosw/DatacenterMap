49e04ec000cda84f61fb5f04682a113a92d784c3
Steffano Heckler <steffano.heckler@cwi.com.br>
2017-12-10 23:56:57 -0200

    feat(andares): adaptadas diretivas para nova tela.

--- FrontEnd/andar/andar.directive.js
+++ FrontEnd/andar/andar.directive.js
@@ -3,14 +3,7 @@ angular.module('app').directive('andarPlanta', ['$compile', function ($compile) 
         restrict: 'A',
         templateUrl: 'image/andar.svg',
         link: function (scope, element, attrs) {
-            var salaElements = element[0].querySelectorAll('.sala');
-            angular.forEach(salaElements, function (path, key) {
-                var salaElement = angular.element(path);
-                salaElement.attr("sala-andar", "");
-                salaElement.attr("dummy-data", "dummyData");
-                salaElement.attr("hover-sala", "hoversala");
-                $compile(salaElement)(scope);
-            })
+            
         }
     }
 }]);
--- FrontEnd/sala/sala.directive.js
+++ FrontEnd/sala/sala.directive.js
@@ -1,25 +1,8 @@
 angular.module('app').directive('salaAndar', ['$compile', function($compile, $location) {
 	return {
         restrict: 'A',
-        scope: {
-            dummyData: "=",
-            hoverSala: "="
-        },
+        scope: {},
         link: function (scope, element, attrs) {
-            scope.elementId = element.attr("id");
-            scope.salaClick = function () {
-                $location.path("/sala/" + scope.elementId)
-                alert(scope.dummyData[scope.elementId].value);
-            };
-            scope.salaMouseOver = function () {
-                scope.hoverSala = scope.elementId;
-                element[0].parentNode.appendChild(element[0]);
-            };
-            element.attr("ng-click", "salaClick()");
-            element.attr("ng-mouseover", "salaMouseOver()");
-            element.attr("ng-class", "{active:hoverSala==elementId}");
-            element.removeAttr("sala-andar");
-            $compile(element)(scope);
         }
     }
 }])
