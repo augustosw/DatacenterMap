19784b30587767f208172dc5935becebaf070529
João Fidellis <joao.fidellis@cwi.com.br>
2017-12-13 16:37:20 -0200

    feat: Exigindo autenticação nas rotas.

--- FrontEnd/app.route.js
+++ FrontEnd/app.route.js
@@ -14,61 +14,51 @@ angular.module('app').config(function ($routeProvider) {
         .when('/edificacao/:id?', {
             controller: 'EdificacaoController',
             templateUrl: 'edificacao/edificacao.html',
-           //TODO : Implementar autenticação
-            // resolve: {
-            //     // define que para acessar esta página deve ser um usuário autenticado (mas não restringe o tipo de permissão)
-            //     autenticado: function (authService) {
-            //         return authService.isAutenticadoPromise();
-            //     }
-            // }
+            resolve: {
+                autenticado: function (authService) {
+                    return authService.isAutenticadoPromise();
+                }
+            }
         })
-        
+
         .when('/andar/:id?', {
             controller: 'AndarController',
             templateUrl: 'andar/andar.html',
-            // resolve: {
-            //     // define que para acessar esta página deve ser um usuário autenticado (mas não restringe o tipo de permissão)
-            //     autenticado: function (authService) {
-            //         return authService.isAutenticadoPromise();
-            //     }
-            // }
+            resolve: {
+                autenticado: function (authService) {
+                    return authService.isAutenticadoPromise();
+                }
+            }
         })
-               
+
         .when('/sala', {
             controller: 'SalaController',
             templateUrl: 'sala/sala.html',
-            //TODO : Implementar autenticação
-            // resolve: {
-            //     // define que para acessar esta página deve ser um usuário autenticado (mas não restringe o tipo de permissão)
-            //     autenticado: function (authService) {
-            //         return authService.isAutenticadoPromise();
-            //     }
-            // }
+            resolve: {
+                autenticado: function (authService) {
+                    return authService.isAutenticadoPromise();
+                }
+            }
         })
 
         .when('/rack', {
             controller: 'RackController',
             templateUrl: 'rack/rack.html',
-            //TODO : Implementar autenticação
-            // resolve: {
-            //     // define que para acessar esta página deve ser um usuário autenticado (mas não restringe o tipo de permissão)
-            //     autenticado: function (authService) {
-            //         return authService.isAutenticadoPromise();
-            //     }
-            // }
+            resolve: {
+                autenticado: function (authService) {
+                    return authService.isAutenticadoPromise();
+                }
+            }
         })
         .when('/equipamento', {
             controller: 'EquipamentoController',
             templateUrl: 'equipamento/equipamento.html',
-            //TODO : Implementar autenticação
-            // resolve: {
-            //     // define que para acessar esta página deve ser um usuário autenticado (mas não restringe o tipo de permissão)
-            //     autenticado: function (authService) {
-            //         return authService.isAutenticadoPromise();
-            //     }
-            // }
+            resolve: {
+                autenticado: function (authService) {
+                    return authService.isAutenticadoPromise();
+                }
+            }
         })
 
         .otherwise('/login');
 });
-
