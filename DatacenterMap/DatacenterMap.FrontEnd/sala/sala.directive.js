c2f98fc71c812db02e50bfe6991668aca87e1f81
Steffano Heckler <steffano.heckler@cwi.com.br>
2017-12-15 04:39:13 -0200

    fix: alinhamento de salas.

--- DatacenterMap/DatacenterMap.FrontEnd/sala/sala.directive.js
+++ DatacenterMap/DatacenterMap.FrontEnd/sala/sala.directive.js
@@ -12,21 +12,8 @@ angular.module('app').directive('salaAndar', ['$compile', function($location) {
             let maximoSalas = scope.$parent.andar.QuantidadeMaximaSalas;
             let indice = scope.$parent.$index;
             
-            let largura = (1200 / maximoSalas) + 100;
+            let largura = 240;
             let altura = 300;
-
-            function calcY() {
-                if (calcX() == 300)
-                    return (maximoSalas - indice) * -300;
-                else 
-                    return (indice * 300);
-            }
-            function calcX() {
-                if (indice > (maximoSalas / 2))
-                    return indice * largura;
-                else
-                    return 0;
-            }
 
             element.attr("y", `${scope.buscarAltura() == 800 ? scope.buscarAltura() - altura : scope.buscarAltura() * altura}`);
             element.attr("x", `${scope.buscarAbcissa() * largura}`);
