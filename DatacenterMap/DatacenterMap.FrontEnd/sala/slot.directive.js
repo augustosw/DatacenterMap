6626e77c30ab545aa0c29dbb2264dc899792c2e6
Steffano Heckler <steffano.heckler@cwi.com.br>
2017-12-16 11:04:37 -0200

    feat: padronização da tela de sala com elementos.

--- DatacenterMap/DatacenterMap.FrontEnd/sala/slot.directive.js
+++ DatacenterMap/DatacenterMap.FrontEnd/sala/slot.directive.js
@@ -4,16 +4,20 @@ angular.module('app').directive('slotSala', ['$compile', function ($compile) {
         templateUrl: 'image/sala.svg',
         scope: {
             buscarAltura: '=',
-            buscarAbcissa: '='
+            buscarAbcissa: '=',
         },
         link: function (scope, element, attrs) {
             const MIN_Y = 100;
             const MIN_X = 100;
 
+            var slot = scope.$parent.slot;
+
             element.attr("y", `${scope.buscarAltura() * MIN_Y}`);
             element.attr("x", `${scope.buscarAbcissa() * MIN_X}`);
             element.attr("height", `100`);
             element.attr("width", `100`);
+
+            slot.Ocupado ? element.attr("fill", "#FF0000") : element.attr("fill", "#00FF00");
             
         }
     }
