26da1fbcdcb7a515aa85c3ebdc298880b5bdf930
Gustavo Rodrigues <gustavo.rodrigues@cwi.com.br>
2017-12-17 05:09:57 -0200

    feat: inicio construcao tooltip para entidades - nao completo

--- DatacenterMap/DatacenterMap.FrontEnd/sala/sala-andar.directive.js
+++ DatacenterMap/DatacenterMap.FrontEnd/sala/sala-andar.directive.js
@@ -19,6 +19,23 @@ angular.module('app').directive('salaAndar', ['$compile', function($location) {
             let ordenada = scope.buscarAltura();
             
             element.attr("style",`transform: translate(${(abcissa * x) + MIN_X}%, ${(ordenada == 100 ? ordenada - y : MIN_Y)}%);`)
+
+            // const MIN_Y = 25;
+            // const MIN_X = 0;
+
+            // let maximoSalas = scope.$parent.andar.QuantidadeMaximaSalas;
+            // let indice = scope.$parent.$index;
+            
+            // let x = 21;
+            // let y = 55;
+
+            //  let abcissa = scope.buscarAbcissa();
+            // let ordenada = scope.buscarAltura();
+
+            // element.attr("style",`left: ${(abcissa * x) + MIN_X}%; top: ${(abcissa == 6 ? ordenada - y : MIN_Y)}%;`)
+
+
+
         }
     }
 }])
